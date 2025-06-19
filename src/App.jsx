a2a1220f486b4c86c6f3467c325cb967686a37c6
Imanshupatel <himanshulakhankiya2003@gmail.com>
2025-06-19 17:58:43 +0530

    add file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Calculator from "./component/Pages/Calculator";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "center", color: "#fff", marginTop: "20px" }}>React Calculator</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
